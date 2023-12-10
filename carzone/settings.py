1cf041881bda50882c1f52f7c340961381451e0a
Volodymyr Popov <smartappsvp@gmail.com>
2023-12-10 01:36:54 +0000

    add media settings

--- carzone/settings.py
+++ carzone/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'qWerty@123',
+        'HOST': 'localhost',
     }
 }
 
@@ -127,3 +130,7 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
