c6a006c80c8e433542ec6fbc69ac074f90bc95cc
Volodymyr Popov <smartappsvp@gmail.com>
2023-12-13 23:38:35 +0000

    fix

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from cars.models import Car
 
 # Create your views here.
 
 
 def cars(request):
     return render(request, 'cars/cars.html')
+
+
+def car_detail(request, id):
+    single_car = get_object_or_404(Car, pk=id)
+
+    data = {
+        'single_car': single_car,
+    }
+    print(id)
+    return render(request, 'cars/car_detail.html', data)
